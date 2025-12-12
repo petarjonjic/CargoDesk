df685d8f1b01ad6b3e58fa4270295efc5a157008
petarjonjic <petarjonjic10@gmail.com>
2025-12-12 20:57:22 +0100

    Initial WinUI 3 project setup with DB and app assets
    Set up CargoDesk WinUI 3 desktop app project with boilerplate code, manifest/config files, and initial workspace state. Added Database.cs for PostgreSQL connectivity and a placeholder async test method. Included launch profiles and multiple logo/splash screen assets. No business logic implemented yet.

---
+++ Software/CargoDesk/CargoDesk/Data/Database.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+
+namespace CargoDesk.Data
+{
+    public static class Database
+    {
+        private const string ConnectionString =
+            "Host=localhost;Port=5433;Database=skladiste-projekt;Username=postgres;Password=jonkee101;";
+
+        public static NpgsqlConnection GetConnection()
+        {
+            return new NpgsqlConnection(ConnectionString);
+        }
+
+        public static async Task TestKonekcijeAsync()
+        {
+            await using var conn = GetConnection();
+            await conn.OpenAsync();
+
+        }
+
+    }
+}
---
+++ Software/CargoDesk/CargoDesk/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using CargoDesk.Data;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace CargoDesk
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+
+ 
+            this.Activated += MainWindow_Activated;
+        }
+
+        private async void MainWindow_Activated(object sender, WindowActivatedEventArgs args)
+        {
+
+            this.Activated -= MainWindow_Activated;
+
+            try
+            {
+                await Database.TestKonekcijeAsync();
+                System.Diagnostics.Debug.WriteLine("Database connection successful.");
+
+ 
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Database connection error: " + ex.Message);
+            }
+        }
+    }
+}
